64f2af5056f39a6b985a8018d7e05dee82c5cb60
Yaafay <muhammad.yaffay@gmail.com>
2025-04-12 22:19:52 +0500

    Added doctor routes and controller for doctor dashboard

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const pool = require("./config/db.js");
-const adminRoutes = require('./routes/admin.routes.js')
-const authRoutes = require('./routes/auth.routes.js')
+const adminRoutes = require("./routes/admin.routes.js");
+const authRoutes = require("./routes/auth.routes.js");
+const doctorRoutes = require("./routes/doctor.routes.js");
 
 dotenv.config();
 
@@ -15,8 +15,9 @@ app.use(cors());
 app.use(express.json());
 
 //Routes
-app.use('/api/admin', adminRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/doctor", doctorRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
